ad997b915e427496a53a1c405f75c1ccc5aa6d03
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-11-16 14:43:50 +0100

    Style för dropdown months

--- top.php
+++ top.php
@@ -18,6 +18,24 @@
 		<li><a href="#"> Color</a></li>
 		<li><a href="#"> Illustration</a></li>
 		<li><a href="#"> Portrait</a></li>
-		<li><a href="#"> Months</a></li>
+		<div class="dropdown">
+			<li>
+				<button class="dropdown-btn">Months <i class="fa fa-caret-down" aria-hidden="true"></i></button>
+			</li>
+			<div class="dropdown-content">
+				<a class="month" href="index.php?month=01">January</a>
+				<a class="month" href="index.php?month=02">February</a>
+				<a class="month" href="index.php?month=03">March</a>
+				<a class="month" href="index.php?month=04">April</a>
+				<a class="month" href="index.php?month=05">May</a>
+				<a class="month" href="index.php?month=06">June</a>
+				<a class="month" href="index.php?month=07">July</a>
+				<a class="month" href="index.php?month=08">August</a>
+				<a class="month" href="index.php?month=09">September</a>
+				<a class="month" href="index.php?month=10">October</a>
+				<a class="month" href="index.php?month=11">November</a>
+				<a class="month" href="index.php?month=12">December</a>
+			</div>
+		</div> <!-- //dropdown -->
 	</ul>
 </div>
