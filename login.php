9c0874e1ddce19dc1374cc30c8c232d994317429
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-11-18 13:23:19 +0100

    Fixat inloggningsformulär (html + css)

--- login.php
+++ login.php
@@ -1,10 +1,17 @@
 <?php
 include "./includes/head.php";
-include "./includes/top.php";
+include "db_connection.php";
 ?>
-
-<form action="logincheck.php" method="post">
-  <input name="email" type="email" value="Enter email">
-  <input name="password" type="password">
-  <button name="send" type="submit">Send</button>
-</form>
+  <main>
+    <section class="login">
+      <form action="logincheck.php" method="post">
+        <label for="email">Your email:</label>
+        <input name="email" type="email">
+        <label for="password">Your password:</label>
+        <input name="password" type="password">
+        <button name="send" type="submit">Login</button>
+      </form>
+    </section>
+  </main>
+</body>
+</html>
