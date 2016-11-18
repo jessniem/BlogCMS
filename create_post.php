ba2a1eae5f36c7c5135b6d38e5b1e88fada477f6
Filip Lindbladh <filiplindbladhh@gmail.com>
2016-11-18 14:10:45 +0100

    createpost.php

--- create_post.php
+++ create_post.php
@@ -1,4 +1,13 @@
 
-
+<?php
+ include "head.php"; 
+ include "top_admin.php";
+?>
 
 hej du är inloggad
+
+<div class="admin-main">
+  <div class="create-post">
+    <h3>Create new post</h3>
+  </div>
+</div>
