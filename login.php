db09d7fddd35000a6dce47a33102e3dff76cdbfb
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-11-18 12:22:15 +0100

    Flyttat ut admin-sidor från admin + skapat formulär i login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+include "./includes/head.php";
+include "./includes/top.php";
+?>
+
+<form action="logincheck.php" method="post">
+  <input name="email" type="email" value="Enter email">
+  <input name="password" type="password">
+  <button name="send" type="submit">Send</button>
+</form>
