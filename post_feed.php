8951214ace122f25930884bd1f258ad230416c10
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-11-21 23:23:46 +0100

    Funktionalitet för paginering

--- post_feed.php
+++ post_feed.php
@@ -4,7 +4,50 @@ require_once "db_connection.php";
 
 $stmt = $conn->stmt_init();
 
-$query = "SELECT posts.*, users.firstName, users.lastName FROM posts LEFT JOIN users ON posts.userid = users.id ORDER BY posts.id DESC";
+// PAGINATION
+
+// posts per page
+$perPage = 5;
+
+// check number of posts in db
+$result = mysqli_query($conn, "SELECT count(*) as total from posts");
+$data = mysqli_fetch_assoc($result);
+$totPosts = $data['total'];
+//echo "antal inlägg: " . $totPosts;
+
+// number of pages
+$totPages = ceil($totPosts/$perPage);
+//echo "<br>antal sidor: " . $totPages;
+
+// get the current page or set a default
+if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
+   $currPage = (int) $_GET["page"];
+} else {
+   // default page
+   $currPage = 1;
+}
+//echo "<br>current page: " . $currPage;
+
+// if current page is greater than total pages
+if ($currPage > $totPages) {
+   // set current page to last page
+   $currPage = $totPages;
+}
+
+// if current page is less than first page...
+if ($currPage < 1) {
+   // set current page to first page
+   $currPage = 1;
+}
+
+// the offset of the list, based on current page
+$offset = ($currPage - 1) * $perPage;
+//echo "<br>Offset: $offset";
+
+// END OF PAGINATION
+
+// get posts from db
+$query = "SELECT posts.*, users.firstName, users.lastName FROM posts LEFT JOIN users ON posts.userid = users.id ORDER BY posts.id DESC LIMIT $offset, $perPage";
 
 if ($stmt->prepare($query)) {
     $stmt->execute();
@@ -66,11 +109,55 @@ foreach ($blogPosts as $post) {
       </article>
     </div> <!-- load-post --> <?php
   }
-}  ?>
+}
 
-<div class="load">
+// PAGINATION LINKS
+// how many links to show
+?>
+<div class="pagination"> <?php
+  $pageRange = 3;
+
+  // if not on page 1, don't show back links
+  if ($currPage > 1) {
+     // show << link to go back to page 1
+     echo " <a href='{$_SERVER['PHP_SELF']}?page=1'><<</a> ";
+     // get previous page num
+     $prevPage = $currPage - 1;
+     // show < link to go back to 1 page
+     echo " <a href='{$_SERVER['PHP_SELF']}?page=$prevPage'><</a> ";
+  }
+
+  // loop to show links to range of pages around current page
+  for ($i = ($currPage - $pageRange); $i < (($currPage + $pageRange) + 1); $i++) {
+     // if it's a valid page number...
+     if (($i > 0) && ($i <= $totPages)) {
+        // current page
+        if ($i == $currPage) {
+           // make current page bold
+           echo " [<b>$i</b>] ";
+        // make links of the other pages
+        } else {
+           // make it a link
+           echo " <a href='{$_SERVER['PHP_SELF']}?page=$i'>$i</a> ";
+        }
+     }
+  }
+
+  // show forward and last page links if not on last page
+  if ($currPage != $totPages) {
+     // get next page
+     $nextPage = $currPage + 1;
+      // link to next page
+     echo " <a href='{$_SERVER['PHP_SELF']}?page=$nextPage'></a> ";
+     // link to the last page
+     echo " <a href='{$_SERVER['PHP_SELF']}?page=$totPages'>>></a> ";
+  } // END OF PAGINATION LINKS ?>
+
+</div> <!-- pagination -->
+
+<!--  <div class="load">
   <a href="#" id="loadMore"><i class="fa fa-angle-double-down" aria-hidden="true"></i></i></a>
-</div> <!-- .load-btn -->
+</div> --> <!-- .load-btn -->
 
 <!-- <p class="totop">
     <a href="#top">Back to top</a>
