f7179d99623e5f61ab7eae9c7d79edfb62b7729a
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-11-23 09:57:54 +0100

    Skapat funktion av “skriva ut inläggen”

--- post_feed.php
+++ post_feed.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "db_connection.php";
+require_once "functions.php";
 
 $stmt = $conn->stmt_init();
 
@@ -60,56 +61,22 @@ while (mysqli_stmt_fetch($stmt)) {
 $blogPosts[] = array ("id" => $id, "title" => $title, "categoryid" => $categoryid, "userid" => $userid, "content" => $content, "image" => $image, "createDate" => $createDate, "isPub" => $isPub, "fname" => $fname, "lname" => $lname);
 }
 
+
+
 // TODO: Fixa sorteringen så att den inte försvinner när klickar på något
 
 // echo out the blogPosts array
 foreach ($blogPosts as $post) {
   $pubmonth = substr($post["createDate"], -4, 2);
-  if (!empty($_GET["month"]) && ($pubmonth == $_GET["month"])) { ?>
-    <div class="load-post">
-      <article class="post">
-        <div class="post-img">
-          <img src="<?php echo $post['image']; ?>" alt="">
-        </div>
-        <div class="post-text">
-          <div> <?php
-          // hämtar kategorinamnet ur db
-          $get_cat = mysqli_fetch_assoc(mysqli_query($conn, "SELECT category FROM categories WHERE id = '{$post["categoryid"]}'")); ?>
-          <p class="tags"><?php echo $get_cat["category"]; ?></p>
-          <h1><?php echo $post["title"]; ?></h1>
-          <div class="blog-content"><?php echo $post["content"]; ?></div>
-        </div>
-        <div>
-          <p class="post-info"><?php echo $post["fname"]. " " .$post["lname"]. ", " .$post["createDate"]; ?></p>
-          <p class="comments">Comments (2)</p>
-        </div>
-      </div> <!-- post-text -->
-      </article>
-    </div> <!-- load-post --> <?php
+  if (!empty($_GET["month"]) && ($pubmonth == $_GET["month"])) {
+    printPost();
     // TODO: Skapa funktion istället för kodupprepning, div load-post är samma här ovan och nedanför
-  } elseif (!isset($_GET["month"])) { ?>
-    <div class="load-post">
-      <article class="post">
-        <div class="post-img">
-          <img src="<?php echo $post['image']; ?>" alt="">
-        </div>
-        <div class="post-text">
-          <div> <?php
-          // hämtar kategorinamnet ur db
-          $get_cat = mysqli_fetch_assoc(mysqli_query($conn, "SELECT category FROM categories WHERE id = '{$post["categoryid"]}'")); ?>
-          <p class="tags"><?php echo $get_cat["category"]; ?></p>
-          <h1><?php echo $post["title"]; ?></h1>
-          <div class="blog-content"><?php echo $post["content"]; ?></div>
-        </div>
-        <div>
-          <p class="post-info"><?php echo $post["fname"]. " " .$post["lname"]. ", " .$post["createDate"]; ?></p>
-          <p class="comments">Comments (2)</p>
-        </div>
-      </div> <!-- post-text -->
-      </article>
-    </div> <!-- load-post --> <?php
+  } elseif (!isset($_GET["month"])) {
+    printPost();
   }
 }
+
+
 
 // PAGINATION LINKS
 // how many links to show
