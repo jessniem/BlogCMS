34eaf88a3f5fe550d65d71e694ab26bcf9d3d6ef
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-11-24 10:23:38 +0100

    Lagt till så att GET month/tag ligger kvar även när man använder pagineringen

--- post_feed.php
+++ post_feed.php
@@ -1,5 +1,16 @@
 <?php
 
+// saves the current selection of tag when using the paging-links
+$get = "";
+if (isset($_GET["month"]) || isset($_GET["tag"])) {
+    if (isset($_GET["month"])) {
+      $get = "month=".$_GET["month"];
+    } elseif ($_GET["tag"]) {
+      $get = "tag=".$_GET["tag"];
+    }
+}
+echo $get;
+
 require_once "db_connection.php";
 require_once "functions.php";
 
@@ -10,15 +21,24 @@ $stmt = $conn->stmt_init();
 // posts per page
 $perPage = 5;
 
+// set $result to the number of posts
+if (isset($_GET["month"]) ) {
+  $month = $_GET["month"];
+  $result = mysqli_query($conn, "SELECT count(*) as total from posts WHERE MONTH(createDate) = $month");
+} elseif (isset($_GET["tag"])) {
+  $tagid = $_GET["tag"];
+  $result = mysqli_query($conn, "SELECT count(*) as total from posts WHERE categoryid = $tagid");
+} else {
+  $result = mysqli_query($conn, "SELECT count(*) as total from posts");
+}
+
 // check number of posts in db
-$result = mysqli_query($conn, "SELECT count(*) as total from posts");
+
 $data = mysqli_fetch_assoc($result);
 $totPosts = $data['total'];
-//echo "antal inlägg: " . $totPosts;
 
 // number of pages
-$totPages = ceil($totPosts/$perPage);
-//echo "<br>antal sidor: " . $totPages;
+$totPages = ceil($totPosts / $perPage);
 
 // get the current page or set a default
 if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
@@ -27,7 +47,6 @@ if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
    // default page
    $currPage = 1;
 }
-//echo "<br>current page: " . $currPage;
 
 // if current page is greater than total pages
 if ($currPage > $totPages) {
@@ -43,37 +62,49 @@ if ($currPage < 1) {
 
 // the offset of the list, based on current page
 $offset = ($currPage - 1) * $perPage;
-//echo "<br>Offset: $offset";
 
 // END OF PAGINATION
 
 // get posts from db
-$query = "SELECT posts.*, users.firstName, users.lastName FROM posts LEFT JOIN users ON posts.userid = users.id ORDER BY posts.id DESC LIMIT $offset, $perPage";
+if (!isset($_GET["tag"]) && (!isset($_GET["month"]))) {
+    $query = "SELECT posts.*, users.firstName, users.lastName, categories.category FROM posts
+   JOIN users ON (users.id = posts.userid)
+   JOIN categories ON (categories.id = posts.categoryid)
+   ORDER BY createDate DESC LIMIT $offset, $perPage";
+
+} elseif (isset($_GET["month"]) ) {
+  $month = $_GET["month"];
+  $query = "SELECT posts.*, users.firstName, users.lastName, categories.category FROM posts
+   JOIN users ON (users.id = posts.userid)
+   JOIN categories ON (categories.id = posts.categoryid) WHERE MONTH(createDate) = $month
+   ORDER BY createDate DESC";
+
+} elseif (isset($_GET["tag"])) {
+  $tagid = $_GET["tag"];
+  $query = "SELECT posts.*, users.firstName, users.lastName, categories.category FROM posts
+   JOIN users ON (users.id = posts.userid)
+   JOIN categories ON (categories.id = posts.categoryid) WHERE categoryid = $tagid
+   ORDER BY createDate DESC";
+}
+
+//var_dump($stmt->prepare($query));
+//var_dump($stmt->error_list);
 
 if ($stmt->prepare($query)) {
     $stmt->execute();
 
-    $stmt->bind_result($id, $title, $categoryid, $userid, $content, $image, $createDate, $isPub, $fname, $lname);
-}
+    $stmt->bind_result($id, $title, $categoryid, $userid, $content, $image, $createDate, $isPub, $fname, $lname, $tag);
 
-//save blog posts in blogPosts array
-while (mysqli_stmt_fetch($stmt)) {
-$blogPosts[] = array ("id" => $id, "title" => $title, "categoryid" => $categoryid, "userid" => $userid, "content" => $content, "image" => $image, "createDate" => $createDate, "isPub" => $isPub, "fname" => $fname, "lname" => $lname);
-}
+    //save blog posts in blogPosts array
+    while (mysqli_stmt_fetch($stmt)) {
+      $blogPosts[] = array ("id" => $id, "title" => $title, "categoryid" => $categoryid, "userid" => $userid, "content" => $content, "image" => $image, "createDate" => $createDate, "isPub" => $isPub, "fname" => $fname, "lname" => $lname, "tag" => $tag);
+    }
+      // TODO: Fixa sorteringen så att den inte försvinner när klickar på något
 
-
-
-// TODO: Fixa sorteringen så att den inte försvinner när klickar på något
-
-// echo out the blogPosts array
-foreach ($blogPosts as $post) {
-  $pubmonth = substr($post["createDate"], -4, 2);
-  if (!empty($_GET["month"]) && ($pubmonth == $_GET["month"])) {
-    printPost();
-    // TODO: Skapa funktion istället för kodupprepning, div load-post är samma här ovan och nedanför
-  } elseif (!isset($_GET["month"])) {
-    printPost();
-  }
+      // echo out the blogPosts array
+      foreach ($blogPosts as $post) {
+          printPost();
+      }
 }
 
 
@@ -87,11 +118,11 @@ foreach ($blogPosts as $post) {
   // if not on page 1, don't show back links
   if ($currPage > 1) {
      // show << link to go back to page 1
-     echo " <a href='{$_SERVER['PHP_SELF']}?page=1'><i class='fa fa-caret-left' aria-hidden='true'></i><i class='fa fa-caret-left' aria-hidden='true'></i></a> ";
+     echo " <a href='{$_SERVER['PHP_SELF']}?$get&page=1'><i class='fa fa-caret-left' aria-hidden='true'></i><i class='fa fa-caret-left' aria-hidden='true'></i></a> ";
      // get previous page num
      $prevPage = $currPage - 1;
      // show < link to go back to 1 page
-     echo " <a href='{$_SERVER['PHP_SELF']}?page=$prevPage'><i class='fa fa-caret-left' aria-hidden='true'></i></a> ";
+     echo " <a href='{$_SERVER['PHP_SELF']}?$get&page=$prevPage'><i class='fa fa-caret-left' aria-hidden='true'></i></a> ";
   }
 
   // loop to show links to range of pages around current page
@@ -105,7 +136,7 @@ foreach ($blogPosts as $post) {
         // make links of the other pages
         } else {
            // make it a link
-           echo " <a href='{$_SERVER['PHP_SELF']}?page=$i'>$i</a> ";
+           echo " <a href='{$_SERVER['PHP_SELF']}?$get&page=$i'>$i</a> ";
         }
      }
   }
@@ -115,12 +146,13 @@ foreach ($blogPosts as $post) {
      // get next page
      $nextPage = $currPage + 1;
       // link to next page
-     echo " <a href='{$_SERVER['PHP_SELF']}?page=$nextPage'<i class='fa fa-caret-right' aria-hidden='true'></i></a> ";
+     echo " <a href='{$_SERVER['PHP_SELF']}?$get&page=$nextPage'<i class='fa fa-caret-right' aria-hidden='true'></i></a> ";
      // link to the last page
-     echo " <a href='{$_SERVER['PHP_SELF']}?page=$totPages'<i class='fa fa-caret-right' aria-hidden='true'></i><i class='fa fa-caret-right' aria-hidden='true'></i></a> ";
+     echo " <a href='{$_SERVER['PHP_SELF']}?$get&page=$totPages'<i class='fa fa-caret-right' aria-hidden='true'></i><i class='fa fa-caret-right' aria-hidden='true'></i></a> ";
   } // END OF PAGINATION LINKS ?>
 
 </div> <!-- pagination -->
+
 
 <!--  <div class="load">
   <a href="#" id="loadMore"><i class="fa fa-angle-double-down" aria-hidden="true"></i></i></a>
@@ -132,7 +164,7 @@ foreach ($blogPosts as $post) {
 <?php
 
 // close db connection
-$stmt->close();
+//$stmt->close();
 $conn->close();
 
 ?>
