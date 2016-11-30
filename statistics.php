ed70861469e6dd60f66e445d952c752fe460bd57
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-11-30 19:47:06 +0100

    Lagt till CSS för statistics.php, dock inte klart för mobil

--- statistics.php
+++ statistics.php
@@ -1,47 +1,48 @@
-<?php 
+<?php
 include "./includes/head.php";
-include "./includes/top_admin.php"; 
-include "./db_connection.php"; 
+include "./includes/top_admin.php";
+include "./db_connection.php";
 
 $stmt = $conn->stmt_init();
 
-$totalP = "SELECT count(*) as total FROM posts WHERE isPub = 1"; 
+$totalP = "SELECT count(*) as total FROM posts WHERE isPub = 1";
 
 if ($stmt->prepare ($totalP)) {
-	$stmt->execute(); 
-	$stmt->bind_result ($totalPosts); 
-	$stmt->fetch(); 
-} 
+	$stmt->execute();
+	$stmt->bind_result($totalPosts);
+	$stmt->fetch();
+}
 
-$totalC = "SELECT count(*) as total FROM comments"; 
+$totalC = "SELECT count(*) as total FROM comments";
 
 if ($stmt->prepare ($totalC)) {
-	$stmt->execute(); 
-	$stmt->bind_result ($totalComments); 
-	$stmt->fetch(); 
-} 
+	$stmt->execute();
+	$stmt->bind_result($totalComments);
+	$stmt->fetch();
+}
 
-$totalS = $totalComments / $totalPosts; 
+$totalS = $totalComments / $totalPosts;
 
 ?>
+<div class="statistics">
 	<div>
 		<div class="box"><?php echo $totalPosts;?></div>
-		<h3>Number of Posts in total</h3>
+		<h3>Number of Posts</h3>
 	</div>
 
 	<div>
 		<div class="box"><?php echo $totalComments;?></div>
-		<h3>Number of comments in total</h3>
+		<h3>Number of comments</h3>
 	</div>
 
 	<div>
 		<div class="box"><?php echo $totalS;?></div>
-		<h3>Average number of comments on posts</h3>
-	</div>			
-
+		<h3>Average comment/post</h3>
+	</div>
+</div>
 <?php
 
- 
 
 
-?> 
+
+?>
