a7703a7a77c0ffb890d1012e821e46470bd94df6
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-11-30 13:39:35 +0100

    Städ och småfix

--- create_post.php
+++ create_post.php
@@ -5,34 +5,37 @@ include_once "./includes/head.php";
 include_once "./includes/top_admin.php";
 require_once "functions.php";
 ?>
-<div class="admin-main">
-  <div class="create-post">
+<main class="admin-main">
+  <section>
     <!-- CREATE NEW BLOG POST -->
     <form method="post" enctype="multipart/form-data">
       <h1>Create new post</h1>
-      <h3>Upload Image</h3>
-      <input name="postImage" type="file" accept="image/*" onchange="loadFile(event)" required="required"><br>
-      <img src="#" style="width:300px; height:auto;" id="output"/>
-      <br>
-      <input type="text" name="alt" value="" placeholder="Image description (alt)">
-      <br>
-      <input type="text" name="title" placeholder="title" required="required"> <br>
-      <textarea name="content" style="width:500px; height:300px;"> </textarea> <br>
-      <h3>Tags</h3>
-        <input type="radio" name="tag" value="3" required="required"> Blackandwhite
-        <input type="radio" name="tag" value="5"> Color
-        <input type="radio" name="tag" value="1"> Illustration
-        <input type="radio" name="tag" value="2"> Portrait
-        <br>
-      <button type="submit" name="saveDraft">Save Draft</button>
-      <button type="submit" name="publish">Publish</button>
+      <div class="upload-image">
+        <h2>Upload Image</h2>
+        <input name="postImage" type="file" accept="image/*" onchange="loadFile(event)" required="required">
+      </div> <!-- .upload-image -->
+      <div class="img-preview">
+        <img src="#" id="output" alt="preview of uploaded image">
+      </div> <!-- .upload-image -->
+      <div class="insert-content">
+        <input type="text" name="alt" value="" placeholder="Image description (alt)">
+        <input type="text" name="title" placeholder="title" required="required">
+        <textarea name="content"></textarea>
+        <h2>Tags</h2>
+        <input type="radio" name="tag" value="3" required="required">Blackandwhite
+        <input type="radio" name="tag" value="5">Color
+        <input type="radio" name="tag" value="1">Illustration
+        <input type="radio" name="tag" value="2">Portrait
+        <button type="submit" name="saveDraft">Save Draft</button>
+        <button type="submit" name="publish">Publish</button>
+      </div> <!-- .insert-content -->
     </form>
-  </div>
-</div>
+  </section>
+</main>
 <?php
-
 if (isset($_POST["publish"])) {
     saveOrPub(1);
   } elseif (isset($_POST["saveDraft"])) {
     saveOrPub(0);
   }
+?>
