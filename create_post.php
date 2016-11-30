11c475167d2131fcf3ca178da5f0763234e7ab99
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-11-30 19:26:01 +0100

    Småfix i formulären

--- create_post.php
+++ create_post.php
@@ -7,40 +7,38 @@ require_once "functions.php";
 ?>
 <main class="admin-main">
   <section>
-    <!-- CREATE NEW BLOG POST -->
+    <!-- FORM: CREATE NEW BLOG POST -->
     <h1>Create new post</h1>
     <form method="post" enctype="multipart/form-data">
-      <div class="content-container">
+      <div class="flex-container">
+        <!-- UPLOAD IMAGE -->
         <div class="upload-image">
-          <h2>Upload Image</h2>
+          <h2>Upload image</h2>
           <input name="postImage" type="file" accept="image/*" onchange="loadFile(event)" required="required">
           <input type="text" name="alt" value="" placeholder="Image description (alt)">
         </div> <!-- .upload-image -->
+        <!-- IMAGE PREVIEW -->
         <div class="img-preview">
           <img src="#" id="output" alt="preview of uploaded image">
         </div> <!-- .img-preview -->
-      </div>
+      </div> <!-- .flex-container -->
+      <!-- POST CONTENT -->
       <div class="post-content">
         <input type="text" name="title" placeholder="Title" required="required">
         <textarea name="content" placeholder="Content"></textarea>
-          <div class="tags">
-            <fieldset>
-              
-              <h3>Tags</h3>
-              <input type="radio" id="blackandwhite" name="tag" value="3">
-              <label for="blackandwhite">Blackandwhite</label>
-
-              <input type="radio" id="color" name="tag" value="5">
-              <label for="color">Color</label>
-
-              <input type="radio" id="illustration" name="tag" value="1">
-              <label for="illustration">Illustration</label>
-
-              <input type="radio" id="portrait" name="tag" value="2">
-              <label for="portrait">Portrait</label>
-
-
-            </fieldset>
+        <!-- SELECT TAGS -->
+        <div class="select-tag">
+          <fieldset>
+            <h3>Select tags</h3>
+            <input type="radio" id="blackandwhite" name="tag" value="3">
+            <label for="blackandwhite">Blackandwhite</label>
+            <input type="radio" id="color" name="tag" value="5">
+            <label for="color">Color</label>
+            <input type="radio" id="illustration" name="tag" value="1">
+            <label for="illustration">Illustration</label>
+            <input type="radio" id="portrait" name="tag" value="2">
+            <label for="portrait">Portrait</label>
+          </fieldset>
         </div>
         <button type="submit" name="saveDraft">Save Draft</button>
         <button type="submit" name="publish">Publish</button>
