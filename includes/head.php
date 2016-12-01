b47fd4b9587bef631044ef4d56bae3ffd03eb915
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-12-01 13:15:34 +0100

    Fixat js för kommentarsgubben

--- includes/head.php
+++ includes/head.php
@@ -9,11 +9,12 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
         <link rel="img/apple-touch-icon" href="apple-touch-icon.png">
+        
         <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/style.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-        <script src="js/preview.js"></script> 
+        <script src="js/preview.js"></script>
     </head>
     <body>
--- js/main.js
+++ js/main.js
@@ -1,38 +1,26 @@
-/*
-Load more content with jQuery - May 21, 2013
-(c) 2013 @ElmahdiMahmoud
-*/
 
-// $(function () {
-//   $(".load-post").slice(0, 5).show();
-//   $("#loadMore").on('click', function (e) {
-//       e.preventDefault();
-//       // console.log($(".load-post:hidden").slice(0, 5));
-//       $(".load-post:hidden").slice(0, 5).slideDown();
-//       if ($(".load-post:hidden").length == 0) {
-//           $("#load").fadeOut('slow');
-//       }
-//       $('html,body').animate({
-//           scrollTop: $(this).offset().top
-//       }, 1000);
-//   });
-// });
-/*
-$('a[href=#top]').click(function () {
-  $('body,html').animate({
-      scrollTop: 0
-  }, 600);
-  return false;
-});
+/**
+* The function sets random color on class comment-fa.
+**/
+window.onload = function(){
 
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 50) {
-      $('.totop a').fadeIn();
-  } else {
-      $('.totop a').fadeOut();
-  }
-});
-*/
+  function ran_col() {
+  var colors = ['f7bc66','e27362','F39EC5', '60939f', '256B7c'];
+
+  for (var i = 0; i < document.getElementsByClassName('comment').length; i++) {
+
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    while (color == last) {
+      color = colors[Math.floor(Math.random() * colors.length)];
+    };
+
+    document.getElementsByClassName('comment-fa')[i].style.color = "#" + color;
+    var last = color;
+    };
+  };
+  ran_col();
+};
+
 
 
 
