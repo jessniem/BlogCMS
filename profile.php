428ead861b1930670b20105d09a56daaf01c95d3
Alice Langerth <alicelangerth@Alices-MacBook-Pro.local>
2016-12-06 15:20:08 +0100

    Fixat så man man uppdatera info på proflie.php. Bild funkar dock inte än :((((((

--- profile.php
+++ profile.php
@@ -4,13 +4,77 @@ require_once "db_connection.php";
 include_once "./includes/head.php";
 include_once "./includes/top_admin.php";
 require_once "functions.php";
-
-
 ?>
 <main class="admin-main">
+  
   <section>
+    <h1>Dina uppgifter</h1>
 
-    <h1>Din Profil</h1> <?php
+    <?php
+    $id = $_SESSION["userId"];
+    $stmt = $conn->stmt_init();
+    $query = "SELECT * FROM users WHERE id = $id";
+
+    if($stmt->prepare($query)) {
+      $stmt->execute();
+
+      $stmt->bind_result($id, $accesslevel, $email, $password, $firstname, $lastname, $profilepic, $description);
+      $stmt->fetch();
+    }
+
+    //TODO: få detta att funka!!!!!!!!!!!!!!!!!!!
+
+    /*  if(isset ($_POST["profilepic"]) ) {
+
+          $targetfolder = './userimg/';
+          $targetname =  $targetfolder . basename ("user-". $id . ".jpg");
+
+          if (move_uploaded_file($_FILES["profilepic"]["tmp_name"], $targetname)) {
+              echo "Filuppladdningen gick bra!";
+          }
+            // $profilepic = sanitizeMySql($conn, $_POST["profilepic"]);
+                
+            $query = "UPDATE users SET profilePic = '{$targetname}' WHERE id = $id"; 
+
+            if ($stmt->prepare($query)) {
+               $stmt->execute();
+              } else {
+               echo mysqli_error();
+              }
+        } */
+
+      if(isset ($_POST["submit"]) ) {
+
+        $email = sanitizeMySql($conn, $_POST["email"]);
+        $description = sanitizeMySql($conn, $_POST["description"]);
+        $stmt = $conn->stmt_init();
+
+        $query2 = "UPDATE users SET email = '{$email}', description = '{$description}' WHERE id = $id"; 
+
+        if ($stmt->prepare($query2)) {
+         $stmt->execute();
+        } else {
+         echo mysqli_error();
+        }
+      }
+    ?>
+    <form method="post" enctype="multipart/form-data">
+      <input name="profilepic" type="file" accept="image/*" onchange="loadFile(event)">
+        <div class="img-preview">
+              <img src="<?php echo $profilepic ?>" id="output" alt="Preview of uploaded image"/>
+        </div>
+      <button type="submit" name="profilepic">Update profile picture</button>
+    </form>
+    <form method="post">
+        <input type="textarea" name="description" value="<?php echo $description; ?>">
+        <input type="email" name="email" value="<?php echo $email; ?>">
+        <button type="submit" name="submit">Save</button>
+    </form>
+
+  </section>
+  
+  <section>
+    <?php
     if (isset($_GET["pw"])) {
         if ($_GET["pw"] == "ok") {
             echo "Your password has been updated!";
@@ -18,9 +82,6 @@ require_once "functions.php";
           echo "Somthing went wrong, your password has not been changed!";
         }
     } ?>
-
-
-    <h2>Dina uppgifter</h2>
 
     <h2>Byt lösenord</h2>
     <form class="" action="changepw.php" method="post">
