e0fe9b1b2e74aedaa028a40826d9fe1a12e0bd49
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-12-06 13:01:55 +0100

    Tagit bort den onödiga dropdownen och döpt om view_posts till edit_posts

---
+++ edit_posts.php
@@ -0,0 +1,56 @@
+<?php session_start()?>
+<?php
+require_once "db_connection.php";
+include_once "./includes/head.php";
+include_once "./includes/top_admin.php";
+require_once "functions.php";
+
+$stmt = $conn->stmt_init();
+
+// Delete post
+if (isset($_GET["delete"])) {
+		$id = $_GET["delete"];
+		$query = "DELETE FROM posts WHERE id = $id";
+		if ($stmt->prepare($query)) {
+      $stmt->execute();
+			echo "Post is deleted";
+		}
+}
+
+// count number of published posts
+$published = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE  isPub = 1");
+$data = mysqli_fetch_assoc($published);
+$totalPub = $data['total'];
+
+// count number of drafts
+$drafts = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE  isPub = 0");
+$data = mysqli_fetch_assoc($drafts);
+$totalDrafts = $data['total'];
+
+?>
+<main class="admin-main">
+	<section class="post-list">
+		<h1>Edit posts</h1> <?php
+		 	if ($totalDrafts > 0) { ?>
+				<h2>Drafts (<?php echo $totalDrafts; ?>)</h2> <?php
+				// LIST OF CREATED DRAFTS
+				listPostAdmin(0);
+		 	}
+			if ($totalPub > 0) { ?>
+				<h2>Published posts (<?php echo $totalPub; ?>)</h2> <?php
+				// LIST OF CREATED DRAFTS
+				listPostAdmin(1);
+			} ?>
+	</section>
+
+	<!-- UPDATE POST -->
+	<?php
+	editPost();
+	?>
+</main>
+<?php
+
+
+
+include "./includes/footer.php";
+?>
--- includes/top_admin.php
+++ includes/top_admin.php
@@ -7,14 +7,7 @@
 	<div class="basemenu">
 		<ul>
 			<li><a class="admin" href="create_post.php">Create post</a></li>
-			<li>
-				<select>
-					<option disabled selected>Edit posts</option>
-					<option><li><a href="view_posts.php">View all posts</a></li></option>
-					<option><li><a href="?view=drafts">View drafts</a></li></option>
-					<option><li><a href="?view=published">View published posts</a></li></option>
-				</select>
-			</li>
+			<li><a class="admin" href="edit_posts.php">Edit posts</a></li>
 			<li><a class="admin" href="statistics.php">Statistics</a></li>
 			<li><a class="admin" href="profile.php">Profile</a></li>
 			<li><a class="admin" href="logout.php">Logout</a></li>
@@ -32,18 +25,9 @@
 		<div class="first">
 			<ul>
 				<li><a href="create_post.php">Create post</a></li>
-				<div class="dropdown">
-					<li>
-						<button class="dropdown-btn">Edit posts <i class="fa fa-caret-down" aria-hidden="true"></i></button>
-					</li>
-					<div class="dropdown-content">
-						<a class="admin" href="view_posts.php">View all posts</a>
-						<a class="admin" href="index.php?view=drafts">View drafts</a>
-						<a class="admin" href="index.php?view=published">View published posts</a>
-					</div>
-				</div> <!-- //dropdown -->
-				<li><a href="statistics.php"> Statistics</a></li>
-				<li><a href="profile.php"> Profile</a></li>
+				<li><a href="edit_posts.php">Edit posts</a></li>
+				<li><a href="statistics.php">Statistics</a></li>
+				<li><a href="profile.php">Profile</a></li>
 			</ul>
 		</div>
 		<div class="logout">
