b3d54a430fdbc303a073af100b8b3baa17165ff5
Filip Lindbladh <filiplindbladhh@gmail.com>
2016-12-14 15:25:32 +0100

    index + footer tabbing / comments

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-  include_once "./includes/head.php";
-  include_once "./includes/top.php";
+include_once "./includes/head.php";
+include_once "./includes/top.php";
 ?>
 <main>
-  <?php
-    require_once "post_feed.php";
-  ?>
-</main>
-
 <?php
-  include_once "./includes/footer.php";
+require_once "post_feed.php";
+?>
+</main>
+<?php
+include_once "./includes/footer.php";
 ?>
