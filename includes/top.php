dfe849dad78934009e9f72367e1fbeb4ea3859ae
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-12-14 09:49:22 +0100

    Intabbat och kommenterat

--- includes/top.php
+++ includes/top.php
@@ -22,40 +22,40 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
 
 <!--- hamburger menu ---->
 <div class="menu2">
-	<header class="hamburgerheader">
-	  <button class="hamburger">&#9776;</button>
-	  <button class="cross">&#735;</button>
-	</header>
-	<div class="basemenu">
-		<ul>
-			<li>
-				<select>
-					<option value="kategorier" disabled selected>Categories</option><?php
+  <header class="hamburgerheader">
+    <button class="hamburger">&#9776;</button>
+    <button class="cross">&#735;</button>
+  </header>
+  <div class="basemenu">
+    <ul>
+      <li>
+        <select>
+          <option value="kategorier" disabled selected>Categories</option><?php
           //print out the tags menu
           for ($i = 0; $i < count($tags); $i++) { ?>
             <option><li><a href="<?php echo $tags["$i"]["href"]; ?>"><?php echo $tags["$i"]["name"]; ?></a></li></option> <?php
           } ?>
-				</select>
-			</li>
-			<li>
-				<select>
-					<option value="months" disabled selected>Months</option>
+        </select>
+      </li>
+      <li>
+        <select>
+          <option value="months" disabled selected>Months</option>
           <div class="dropdown-content"> <?php
-    				$stmt = $conn->stmt_init();
+            $stmt = $conn->stmt_init();
             // Check which months that have posts
-    				for($i = 0; $i<12; $i++) {
+            for($i = 0; $i<12; $i++) {
               $month = $months["$i"];
-    					$query = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE isPub = 1 AND MONTHNAME(createDate) = '$month'");
-    					$data = mysqli_fetch_assoc($query);
-    					$monthsum = $data['total'];
-    					if($monthsum > 0) { ?>
-    						<option><a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></a></option><?php
-    					}
-    				}?>
-    			</div>
-				</select>
-			</li>
-			<li><hr/></li>
+              $query = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE isPub = 1 AND MONTHNAME(createDate) = '$month'");
+              $data = mysqli_fetch_assoc($query);
+              $monthsum = $data['total'];
+              if($monthsum > 0) { ?>
+                <option><a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></a></option><?php
+              }
+            } ?>
+          </div>
+        </select>
+      </li>
+      <li><hr/></li>
       <li><a href="about_me.php">About me</a></li>
       <li><a href="index.php">Home</a></li> <?php
       // options only visible for logged in users
@@ -65,8 +65,8 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
         } ?>
         <li><a href="logout.php">Logout</a></li> <?php
       } ?>
-		</div>
-		</ul>
+    </div>
+  </ul>
 </div> <!-- /hamburger menu -->
 
 
@@ -87,47 +87,47 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
 
   <div class="right">
     <ul>
-  		<li><a href="about_me.php">About me</a></li> <?php
+      <li><a href="about_me.php">About me</a></li> <?php
       //logout link for logged in users
       if (isset($_SESSION["logged_in"])) { ?>
         <li><a href="logout.php">Logout</a></li> <?php
       } ?>
-  		<li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i></a></li>
-  	</ul>
+      <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+    </ul>
   </div>
 </div>
 <!-- Banner -->
 <div class="banner">
-	<img class="mobile-logo" src="./img/logo_mobile.png" alt="logo">
-	<img class="desktop-logo" src="./img/logo_desktop.png" alt="logo">
+  <img class="mobile-logo" src="./img/logo_mobile.png" alt="logo">
+  <img class="desktop-logo" src="./img/logo_desktop.png" alt="logo">
 </div>
 <div class="bluefiller">
 </div>
 <div class="menu filter-menu">
-	<ul> <?php
+  <ul> <?php
   //print out the tags menu
   for ($i = 0; $i < count($tags); $i++) { ?>
     <li><a href="<?php echo $tags["$i"]["href"]; ?>"><?php echo $tags["$i"]["name"]; ?></a></li> <?php
   } ?>
-		<div class="dropdown">
-			<li>
-				<button class="dropdown-btn">Months <i class="fa fa-caret-down" aria-hidden="true"></i></button>
-			</li>
-			<div class="dropdown-content"> <?php
-				$stmt = $conn->stmt_init();
-        // Check which months that have posts and not
-				for($i = 0; $i<12; $i++) {
-          $month = $months["$i"];
-					$query = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE isPub = 1 AND MONTHNAME(createDate) = '$month'");
-					$data = mysqli_fetch_assoc($query);
-					$monthsum = $data['total'];
-					if($monthsum == 0) { ?>
-					<p class="month empty" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></p> <?php
-					} else { ?>
-						<a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></a><?php
-					}
-				}?>
-			</div>
-		</div> <!-- /dropdown -->
-	</ul>
+  <div class="dropdown">
+    <li>
+      <button class="dropdown-btn">Months <i class="fa fa-caret-down" aria-hidden="true"></i></button>
+    </li>
+    <div class="dropdown-content"> <?php
+      $stmt = $conn->stmt_init();
+      // Check which months that have posts and not
+      for($i = 0; $i<12; $i++) {
+      $month = $months["$i"];
+      $query = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE isPub = 1 AND MONTHNAME(createDate) = '$month'");
+      $data = mysqli_fetch_assoc($query);
+      $monthsum = $data['total'];
+      if($monthsum == 0) { ?>
+        <p class="month empty" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></p> <?php
+      } else { ?>
+        <a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></a><?php
+      }
+    }?>
+    </div>
+  </div> <!-- /dropdown -->
+  </ul>
 </div>
