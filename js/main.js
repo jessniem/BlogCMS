6197ec5e1f2707555da21decfcec902e0f3de944
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-12-14 16:12:51 +0100

    Fixat intabbningen

--- js/main.js
+++ js/main.js
@@ -4,21 +4,22 @@
 **/
 window.onload = function(){
 
-  function ran_col() {
-  var colors = ['f7bc66','e27362','F39EC5', '60939f', '256B7c'];
+    function ran_col() {
+        var colors = ['f7bc66','e27362','F39EC5', '60939f', '256B7c'];
 
-  for (var i = 0; i < document.getElementsByClassName('comment').length; i++) {
+        for (var i = 0; i < document.getElementsByClassName('comment').length; i++) {
 
-    var color = colors[Math.floor(Math.random() * colors.length)];
-    while (color == last) {
-      color = colors[Math.floor(Math.random() * colors.length)];
+            var color = colors[Math.floor(Math.random() * colors.length)];
+
+            while (color == last) {
+                color = colors[Math.floor(Math.random() * colors.length)];
+            };
+
+            document.getElementsByClassName('comment-fa')[i].style.color = "#" + color;
+            var last = color;
+        };
     };
-
-    document.getElementsByClassName('comment-fa')[i].style.color = "#" + color;
-    var last = color;
-    };
-  };
-  ran_col();
+    ran_col();
 };
 
 
@@ -28,16 +29,21 @@ window.onload = function(){
 /* HAMBURGER */
 $( ".cross" ).hide();
 $( ".basemenu" ).hide();
+
 $( ".hamburgericon" ).click(function() {
-$( ".basemenu" ).slideToggle( "slow", function() {
-$( ".hamburgericon" ).hide();
-$( ".cross" ).show();
-});
+
+    $( ".basemenu" ).slideToggle( "slow", function() {
+
+        $( ".hamburgericon" ).hide();
+        $( ".cross" ).show();
+    });
 });
 
 $( ".cross" ).click(function() {
-$( ".basemenu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburgericon" ).show();
-});
+
+    $( ".basemenu" ).slideToggle( "slow", function() {
+
+        $( ".cross" ).hide();
+        $( ".hamburgericon" ).show();
+    });
 });
