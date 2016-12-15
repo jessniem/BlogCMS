0e5b4d9dbb7abe899fff4e168528f3630affa469
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-12-15 10:23:41 +0100

    Ändrat länkar till ”home” och ”about me” (istället för ”contact”), samt fixat tabbning och kommentarer

--- includes/footer.php
+++ includes/footer.php
@@ -1,37 +1,34 @@
-<footer>
-    <div class="hide">
-        <ul>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-    <div class="social">
-        <a href="https://www.facebook.com/angelina.fredriksson"><i class="fa fa-facebook-official"></i></a>
-        <a href="https://www.instagram.com/angelinaf/"><i class="fa fa-instagram"></i></a>
-        <a href="http://angelinafredriksson-blog.tumblr.com/"><i class="fa fa-tumblr-square"></i></a>
-    </div>
-    <div>
-         &copy; Angelina Fredriksson 2016
-    </div>
-</footer>
+    <footer>
+        <div class="hide">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about_me.php">About me</a></li>
+            </ul> <!-- /hide -->
+        </div>
+        <div class="social">
+            <a href="https://www.facebook.com/angelina.fredriksson"><i class="fa fa-facebook-official"></i></a>
+            <a href="https://www.instagram.com/angelinaf/"><i class="fa fa-instagram"></i></a>
+            <a href="http://angelinafredriksson-blog.tumblr.com/"><i class="fa fa-tumblr-square"></i></a>
+        </div> <!-- /social -->
+        <div>
+             &copy; Angelina Fredriksson 2016
+        </div>
+    </footer>
 
+    <!-- SCRIPT -->
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js?ver=1"></script>
 
-
-<!-- SCRIPT -->
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js?ver=1"></script>
-
-<!-- GOOGLE ANALYTICS: CHANGE UA-XXXXX-X TO BE YOUR SITE'S ID. -->
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
-
+    <!-- GOOGLE ANALYTICS: CHANGE UA-XXXXX-X TO BE YOUR SITE'S ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
 </body>
 </html>
