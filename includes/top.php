8adba27e0c47e77f56506be0f9d2a378d7034209
jessniem <jessica@Jessicas-MacBook-Pro.local>
2016-12-19 13:56:56 +0100

    länkat bilden, html-fix och språk i head

--- includes/top.php
+++ includes/top.php
@@ -35,9 +35,9 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
                         <option value="categories" disabled selected>Categories &#9662;</option>
                         <?php
                         for ($i = 1; $i < count($tags); $i++) { ?>
-                           <option value="<?php echo $tags[$i]["num"]; ?>"><?php echo $tags["$i"]["name"]; ?></option> 
+                           <option value="<?php echo $tags[$i]["num"]; ?>"><?php echo $tags["$i"]["name"]; ?></option>
                            <?php
-                        } 
+                        }
                         ?>
                     </select>
                 </li>
@@ -53,13 +53,13 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
                                 $query = mysqli_query($conn, "SELECT count(*) as total FROM posts WHERE isPub = 1 AND MONTHNAME(createDate) = '$month'");
                                 $data = mysqli_fetch_assoc($query);
                                 $monthsum = $data['total'];
-                                if($monthsum > 0) { 
+                                if($monthsum > 0) {
                                     ?>
                                     <option value="<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></option>
                                 <?php
                                 }
-                            } 
-                            ?>     
+                            }
+                            ?>
                     </select>
                 </li>
                 <li><hr/></li>
@@ -68,12 +68,12 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
                 <?php
                 // ONLY FOR LOGGED IN USERS
                 if (isset($_SESSION["logged_in"])) {
-                    for ($i = 0; $i < count($admin); $i++) { 
+                    for ($i = 0; $i < count($admin); $i++) {
                         ?>
                         <li><a href="<?php echo $admin["$i"]["href"]; ?>"><?php echo $admin["$i"]["name"]; ?></a></li> <?php
-                    } 
+                    }
                     ?>
-                    <li><a href="logout.php">Logout</a></li> 
+                    <li><a href="logout.php">Logout</a></li>
                     <?php
                 } else {
                     ?>
@@ -89,40 +89,40 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
 
 <!-- TOPMENU (DESKTOP) -->
 <div class="menu top-menu">
-    <div class="left"> 
+    <div class="left">
         <?php
         // ADMIN OPTIONS
-        if (isset($_SESSION["logged_in"])) { 
+        if (isset($_SESSION["logged_in"])) {
             ?>
-            <ul> 
+            <ul>
             <?php
-            for ($i = 0; $i < count($admin); $i++) { 
+            for ($i = 0; $i < count($admin); $i++) {
                 ?>
-                <li><a href="<?php echo $admin["$i"]["href"]; ?>"><?php echo $admin["$i"]["name"]; ?></a></li> 
+                <li><a href="<?php echo $admin["$i"]["href"]; ?>"><?php echo $admin["$i"]["name"]; ?></a></li>
                 <?php
-            } 
+            }
             ?>
-            </ul> 
+            </ul>
             <?php
-        } else { 
+        } else {
             ?>
             <ul>
                 <li><a href="login.php">Login</a></li>
-            </ul> 
+            </ul>
             <?php
-        } 
+        }
         ?>
     </div> <!-- /left -->
     <div class="right">
         <ul>
-            <li><a href="about_me.php">About me</a></li> 
+            <li><a href="about_me.php">About me</a></li>
             <?php
             // LOGOUT (ONLY FOR LOGGED IN USERS)
-            if (isset($_SESSION["logged_in"])) { 
+            if (isset($_SESSION["logged_in"])) {
                 ?>
-                <li><a href="logout.php">Logout</a></li> 
+                <li><a href="logout.php">Logout</a></li>
             <?php
-            } 
+            }
             ?>
             <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i></a></li>
         </ul>
@@ -133,31 +133,32 @@ $months = array("January", "February", "March", "April", "May", "June", "July", 
 $currentfile = strrchr($_SERVER['PHP_SELF'], "/");
 if (($currentfile == "/index.php") ||
     ($currentfile == "/about_me.php") ||
-    ($currentfile == "/comments.php")) { 
+    ($currentfile == "/comments.php")) {
     ?>
-    
+
     <!-- BANNER -->
-    <div class="banner">
-        <img class="mobile-logo" src="./img/logo_mobile.png" alt="logo">
-        <img class="desktop-logo" src="./img/logo_desktop.png" alt="logo">
-    </div> <!-- /banner -->
-    <div class="bluefiller">
-    </div> <!-- /bluefiller -->
+    <a href="index.php">
+        <div class="banner">
+            <img class="mobile-logo" src="./img/logo_mobile.png" alt="logo">
+            <img class="desktop-logo" src="./img/logo_desktop.png" alt="logo">
+        </div> <!-- /banner -->
+    </a>
+    <div class="bluefiller"></div>
 
     <!-- FILTER MENU -->
     <div class="menu filter-menu">
             <!-- FILTER OPTION: CATEGORIES -->
             <?php
-            for ($i = 0; $i < count($tags); $i++) { 
+            for ($i = 0; $i < count($tags); $i++) {
                 ?>
                 <a href="<?php echo $tags["$i"]["href"]; ?>"><?php echo $tags["$i"]["name"]; ?></a>
             <?php
-            } 
+            }
             ?>
             <!-- FILTER OPTION: MONTHS -->
             <div class="dropdown">
                 <button class="dropdown-btn">Months <i class="fa fa-caret-down" aria-hidden="true"></i></button>
-                <div class="dropdown-content"> 
+                <div class="dropdown-content">
                     <?php
                     $stmt = $conn->stmt_init();
                     // LOOP OUT MONTHS WITH AND WITHOUT POSTS
@@ -167,16 +168,16 @@ if (($currentfile == "/index.php") ||
                         $data = mysqli_fetch_assoc($query);
                         $monthsum = $data['total'];
                         // MAKE EMPTY MONTHS UNAVAILABLE
-                        if($monthsum == 0) { 
+                        if($monthsum == 0) {
                             ?>
-                            <p class="month empty" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?></p> 
+                            <p class="month empty"><?php echo $months[$i]; ?></p>
                         <?php
-                        } else { 
+                        } else {
                             ?>
-                            <a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?> </a> 
+                            <a class="month" href="index.php?month=<?php echo ($i+1); ?>"><?php echo $months[$i]; ?> </a>
                             <?php
                         }
-                    } 
+                    }
                     ?>
                 </div> <!-- /dropdown-content -->
             </div> <!-- /dropdown -->
