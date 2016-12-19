657053669090d60a45b41564ab4241cd678e10ec
petrahagman <phagman@Ps-MacBook-Pro.local>
2016-12-19 11:19:13 +0100

    Tabbning/kommentarer

--- includes/head.php
+++ includes/head.php
@@ -5,12 +5,12 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Angelina Fredriksson</title>
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/style.css">
+        <!-- SCRIPT -->
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
         <script src="js/preview.js"></script>
     </head>
